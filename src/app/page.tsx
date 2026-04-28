b98b5961a6baa0e0130f4b1184be3c1ac0cf113c
Agil Mammadov <mammadovagil@tutamail.com>
2026-04-28 23:21:55 +0400

    style: set dark theme and bg image (#9)
    * style: set dark theme
    
    * style: set bakcgound patter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,17 @@ import { Input } from "@/components/ui/input"
 
 export default function Home() {
 	return (
-		<main className="flex flex-1 flex-col items-center justify-center gap-8 px-4">
-			<h1 className="text-4xl font-bold tracking-tight">BekarsSpace</h1>
-			<form className="flex w-full max-w-sm gap-2">
-				<Input type="email" placeholder="your@email.com" required />
-				<Button type="submit">Subscribe</Button>
-			</form>
-		</main>
+		<>
+			<div className="absolute inset-0 bg-[url('/logo.svg')] bg-cover bg-center brightness-20 blur-lg -z-100" />
+			<main className="flex flex-1 flex-col items-center justify-center gap-8 px-4">
+				<h1 className="text-4xl font-bold tracking-tight">
+					BekarsSpace
+				</h1>
+				<form className="flex w-full max-w-sm gap-2">
+					<Input type="email" placeholder="your@email.com" required />
+					<Button type="submit">Subscribe</Button>
+				</form>
+			</main>
+		</>
 	)
 }
